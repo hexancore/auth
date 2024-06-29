c6ea6bb8d31d3c6e01deffe87c822abb2aad53fb
mararok <5163714+Mararok@users.noreply.github.com>
2024-06-29 12:00:58 +0200

    fix: sha256() now returns sha256

--- src/Util/HashUtil.ts
+++ src/Util/HashUtil.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 export function sha256(data: string): string {
-  const hash = crypto.createHash('sha1');
+  const hash = crypto.createHash('sha256');
   hash.update(data);
   return hash.digest('hex');
 }
